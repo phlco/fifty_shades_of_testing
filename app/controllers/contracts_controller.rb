4513cc0209fa4f1cea248e9599b0fa553d962eb4
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2015-02-27 09:54:52 -0500

    adds passing welcome specs

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,4 +1,7 @@
 class ContractsController < ApplicationController
+  def index
+  end
+
   def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
 
+  get '/contract' => 'contracts#index'
 
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 end
